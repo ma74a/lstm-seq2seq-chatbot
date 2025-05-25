8e786f500f1cba970adc6cf2c895336889c65ffd
mahmoud etman <metman835@gmail.com>
2025-05-25 16:08:26 +0300

    updated config file

--- utils/config.py
+++ utils/config.py
@@ -6,21 +6,21 @@ class Config:
     DATA_PATH = "/home/mahmoud/final_project/lstm-seq2seq-chatbot/data/furniture_chatbot_dataset.json"
     
     # LSTM Model Parameters
-    EMBED_SIZE = 256
-    HIDDEN_SIZE = 256
+    EMBED_SIZE = 512
+    HIDDEN_SIZE = 512
     NUM_LAYER = 2
-    DROPOUT = 0.5
+    DROPOUT = 0.7
     
     # Hyper Parameters
     BATCH_SIZE = 64
-    EPOCHS = 100
+    EPOCHS = 200
     LEARNING_RATE = 0.0001
     TEACHER_FORCING_RATIO = 0.7
     
     # Tokenizer Parameters
     MAX_INPUT_LENGTH = 128
     MAX_OUTPUT_LENGTH = 256
-    VOCAB_SIZE = 5000
+    VOCAB_SIZE = 4000
     PAD_TOKEN = "[PAD]"
     UNK_TOKEN = "[UNK]"
     SOS_TOKEN = "[SOS]"
